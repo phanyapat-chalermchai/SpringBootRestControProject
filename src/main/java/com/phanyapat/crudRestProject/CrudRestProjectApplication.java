f4e5698e0ab65ba786d78976a428d9af60cb599e
Phanyapat Chalermchai <81134953+phanyapat-chalermchai@users.noreply.github.com>
2024-04-07 19:47:25 +0700

    add ProjectRestController

---
+++ src/main/java/com/phanyapat/crudRestProject/CrudRestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.phanyapat.crudRestProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRestProjectApplication.class, args);
+	}
+
+}
